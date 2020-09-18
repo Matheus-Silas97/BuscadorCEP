686abef2cb88ab3a946713f64066928452575f77
Matheus-Silas97 <matheus.silas123@gmail.com>
2020-09-18 10:18:02 -0300

    Adicionado opção de abrir o Google Maps com as coordenadas do CEP selecionado

--- app/src/main/java/com/matheussilas97/buscadordecep/ui/MainActivity.kt
+++ app/src/main/java/com/matheussilas97/buscadordecep/ui/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.matheussilas97.buscadordecep.ui
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -11,6 +13,9 @@ import com.matheussilas97.buscadordecep.api.CepViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var longitude: String? = null
+    private var latitude: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,7 +26,7 @@ class MainActivity : AppCompatActivity() {
             val cep = edit_cep.text.toString()
 
             if (edit_cep.text.toString().isEmpty()) {
-                Toast.makeText(this, "Preencha o campo CEP", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, R.string.preencha_campo, Toast.LENGTH_SHORT).show()
             } else {
                 viewModel.searchCep(cep).observe(this, Observer { data ->
                     if (data != null) {
@@ -37,12 +42,33 @@ class MainActivity : AppCompatActivity() {
                         longitude_text.text = data.lng
                         ddd_text.text = data.ddd
                         city_code_text.text = data.city_ibge
+
+                        latitude = data.lat
+                        longitude = data.lng
                     } else {
                         cep_error.visibility = View.VISIBLE
                         layout_info.visibility = View.GONE
+                        latitude = null
+                        longitude = null
                     }
                 })
             }
+        }
+        button.setOnClickListener {
+            openMaps()
+        }
+    }
+
+    private fun openMaps() {
+        if (latitude != null && longitude != null) {
+            val gmmIntentUri = Uri.parse("geo:?q=$latitude,$longitude")
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            mapIntent.resolveActivity(packageManager)?.let {
+                startActivity(mapIntent)
+            }
+        } else {
+            Toast.makeText(this, R.string.pesquise_cep, Toast.LENGTH_SHORT).show()
         }
     }
 }
